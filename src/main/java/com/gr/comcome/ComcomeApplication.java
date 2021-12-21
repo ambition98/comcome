9d9ddf25735aa4adc7097b15af476bafbae87aa1
YJ_Lee <ambition65@naver.com>
2021-12-21 17:36:53 +0900

    First commmit

---
+++ src/main/java/com/gr/comcome/ComcomeApplication.java
@@ -0,0 +1,13 @@
+package com.gr.comcome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComcomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComcomeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gr/comcome/ComcomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gr.comcome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComcomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
